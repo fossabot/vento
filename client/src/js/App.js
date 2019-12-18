cfa0e92c3b2619eb9d4085cd6c90b25189573a6c
Tapas Adhikary <tapas.adhikary@gmail.com>
2019-12-18 10:23:08 +0530

    Created a form to test the REST Service call from UI

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,9 +1,34 @@
 import React from "react";
 
+import useForm from 'react-hook-form';
 
 const App = () => {
+  const { register, handleSubmit, errors } = useForm(); // initialise the hook
+  const onSubmit = data => {
+    console.log(data);
+  };
+
   return (
-    <h1>APP is Running!</h1>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <input name="firstname" ref={register} /> {/* register an input */}
+      </div>
+
+      <div>
+        <label htmlFor="lastname">Last Name</label>
+        <input name="lastname" ref={register({ required: true })} />
+        <span className="error">{errors.lastname && 'Last name is required.'}</span>
+      </div>
+      
+      <div>
+        <label htmlFor="age">Age</label>
+        <input name="age" ref={register({ pattern: /\d+/ })} />
+        <span className="error">{errors.age && 'Please enter number for age.'}</span>
+      </div>
+      
+      <input type="submit" />
+    </form>
   );
 };
 export default App;
