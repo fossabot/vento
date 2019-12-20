5b57821ada0efe836133c7bfe4f89171d987f614
Tapas Adhikary <tapas.adhikary@gmail.com>
2019-12-20 08:06:14 +0530

    Added REST APIs for Hardware Assets

--- rest-services/api/controllers/HardwareAssetController.js
+++ rest-services/api/controllers/HardwareAssetController.js
@@ -5,91 +5,71 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+
+const APIUtil = require('../utils/APIUtil');
 module.exports = {
-    id: {
-        type: 'string',
-        required: true,
-        unique: true
+    create: async function (req, res) {
+        let hardwareAssets = req.param('hardwareAssets');
+        let payload = APIUtil.getCreatePayload(hardwareAssets);
+        let createdHardwareAssets = await HardwareAsset.createEach(payload).fetch();
+        sails.log(`Created the hardwareAsset(s) successfully: ${JSON.stringify(createdHardwareAssets)}`);
+        return res.json({ hardwareAssets: createdHardwareAssets });
     },
-    asset_id: {
-        type: 'string',
-        required: true,
-        unique: true
+
+    get: async function (req, res) {
+        let query = req.query;
+        let hardwareAssets = await HardwareAsset.find(query);
+        sails.log(`Fetched all the hardwareAssets successfully: ${JSON.stringify(hardwareAssets)}`);
+        return res.json({ hardwareAssets: hardwareAssets });
     },
-    location: {
-        type: 'string',
-        required: true,
-        unique: false
+    getById: async function (req, res) {
+        let id = req.param('id');
+        let hardwareAsset = await HardwareAsset.findOne({ id: id });
+        sails.log(`Fetched the hardwareAsset details by ${id}: ${JSON.stringify(hardwareAsset)}`);
+        return res.json({ hardwareAsset: hardwareAsset });
     },
-    type: {
-        type: 'string',
-        required: true,
-        unique: false
+
+    update: async function (req, res) {
+        let id = req.param('id');
+        let payload = req.param('hardwareAsset');
+        let updatedHardwareAsset = await HardwareAsset.updateOne({ id: id })
+            .set(payload);
+
+        if (updatedHardwareAsset) {
+            sails.log(`Updated the hardwareAsset of id ${id} with ${JSON.stringify(payload)}`);
+        }
+        else {
+            sails.log(`The database does not contain a hardwareAsset having id ${id}.`);
+        }
+
+        return res.json({ hardwareAsset: updatedHardwareAsset });
     },
-    ip: {
-        type: 'string',
-        required: true,
-        unique: true
+
+    delete: async function (req, res) {
+        let id = req.param('id');
+        let deletedHardwareAsset = await HardwareAsset.destroyOne({id: id})
+        if (deletedHardwareAsset) {
+            sails.log(`Deleted hardwareAsset with id: ${id}.`);
+        } else {
+            sails.log(`The database does not have a hardwareAsset with id: ${id}.`);
+        }
+
+        return res.json({ hardwareAsset: deletedHardwareAsset });
     },
-    server_tag: {
-        type: 'string',
-        required: true,
-        unique: false
-    },
-    resource_details: {
-        type: 'string',
-        required: true,
-        unique: false
-    },
-    contract_end_date: {
-        type: 'string',
-        required: true,
-        unique: false
-    },
-    owner: {
-        type: 'string',
-        required: true,
-        unique: false
-    },
-    consumer: {
-        type: 'string',
-        required: true,
-        unique: false
-    },
-    state: {
-        type: 'boolean',
-        required: true,
-        unique: false
-    },
-    os_version: {
-        type: 'string',
-        required: true,
-        unique: false
-    },
-    os_patch: {
-        type: 'string',
-        required: false,
-        unique: false
-    },
-    notes: {
-        type: 'string',
-        required: false,
-        unique: false
-    },
-    product_id: {
-        type: 'string',
-        required: true,
-        unique: false
-    },
-    department_id: {
-        type: 'string',
-        required: true,
-        unique: false
-    },
-    notification_id: {
-        type: 'string',
-        required: true,
-        unique: true
+
+    deleteMulti: async function (req, res) {
+        let ids = req.param('ids');
+        let deletedRecords = await HardwareAsset.destroy({
+            id: { in: ids }
+        }).fetch();
+        
+        if (deletedRecords) {
+            sails.log(`All the hardwareAsset recorts deleted successfully.`);
+        } else {
+            sails.log(`Problem in deleting all the hardwareAsset records`);
+        }
+        
+        return res.json({ count: deletedRecords.length });
     }
 };
 
--- rest-services/api/models/HardwareAsset.js
+++ rest-services/api/models/HardwareAsset.js
@@ -9,7 +9,92 @@ module.exports = {
 
   attributes: {
 
-    
+    id: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+    asset_id: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+    location: {
+      type: 'string',
+      required: true,
+      unique: false
+    },
+    type: {
+      type: 'string',
+      required: true,
+      unique: false
+    },
+    ip: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+    server_tag: {
+      type: 'string',
+      required: true,
+      unique: false
+    },
+    resource_details: {
+      type: 'string',
+      required: true,
+      unique: false
+    },
+    contract_end_date: {
+      type: 'string',
+      required: true,
+      unique: false
+    },
+    owner: {
+      type: 'string',
+      required: true,
+      unique: false
+    },
+    consumer: {
+      type: 'string',
+      required: true,
+      unique: false
+    },
+    state: {
+      type: 'boolean',
+      required: true,
+      unique: false
+    },
+    os_version: {
+      type: 'string',
+      required: true,
+      unique: false
+    },
+    os_patch: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    notes: {
+      type: 'string',
+      required: false,
+      unique: false
+    },
+    product_id: {
+      type: 'string',
+      required: true,
+      unique: false
+    },
+    department_id: {
+      type: 'string',
+      required: true,
+      unique: false
+    },
+    notification_id: {
+      type: 'string',
+      required: true,
+      unique: true
+    }
+
 
   },
 
