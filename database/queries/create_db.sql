ed3964d48c644bb2d19c84b07d6963040663a216
Tapas Adhikary <tapas.adhikary@gmail.com>
2019-12-20 10:38:34 +0530

    create db script should execute as vento user

--- database/queries/create_db.sql
+++ database/queries/create_db.sql
@@ -4,7 +4,7 @@
 
 CREATE DATABASE inventory
     WITH 
-    OWNER = postgres
+    OWNER = vento
     ENCODING = 'UTF8'
     LC_COLLATE = 'English_United States.1252'
     LC_CTYPE = 'English_United States.1252'
