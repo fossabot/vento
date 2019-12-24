35c8255ebd8dd2d53247546ca06e5f421e1c8674
Tapas Adhikary <tapas.adhikary@gmail.com>
2019-12-24 12:33:55 +0530

    We do not need logout to be exposed as route

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Switch} from "react-router-dom";
 
 import Login from './Login';
 import Home from './Home';
-import Logout from './Logout';
 
 import PrivateRoute from './routes/PrivateRoute';
 import PublicRoute from './routes/PublicRoute';
@@ -18,8 +17,7 @@ const App = () => {
             <PublicRoute restricted={ false } component={ Login } path="/login" exact />
             <PrivateRoute component={ Home } path="/" exact />
             <PrivateRoute component={ Home } path="/home" exact />
-            <PrivateRoute component={ Logout } path="/logout" exact />
-          </Switch>
+            </Switch>
       </BrowserRouter>
     </React.Fragment>
   );
