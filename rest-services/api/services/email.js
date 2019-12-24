3c05636c5700a2760c2f805a155d6b08001ddf03
Tapas Adhikary <tapas.adhikary@gmail.com>
2019-12-24 08:41:16 +0530

    Sails lift was not working because a reference was deleted. Now fixed

--- rest-services/api/services/email.js
+++ rest-services/api/services/email.js
@@ -1,5 +1,5 @@
 let nodemailer = require('nodemailer');
-let joker = require('./joker');
+// let joker = require('./joker');
 
 let transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -25,16 +25,16 @@ module.exports.sendEmail = async function() {
     })
     console.log('Subscribed Emails to send @ ' + new Date(), subsEmails);
 
-    const randomeJoke = await joker.getRandomOne();
-    const jokeContent = randomeJoke[0].content;
-    console.log('Random Joke to send', randomeJoke, jokeContent);
+    // const randomeJoke = await joker.getRandomOne();
+    // const jokeContent = randomeJoke[0].content;
+    // console.log('Random Joke to send', randomeJoke, jokeContent);
 
 
     let mailOptions = {
         from: 'emailbotgreenrooots@gmail.com',
         to: subsEmails.join(),
         subject: 'Jokking: Joke of the Day!',
-        text: jokeContent
+        text: 'Some Text'
     };
     
 
