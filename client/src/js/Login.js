ed43986991968a8d90c302565db99fa748941288
Nirmal Kumar <nirmalkc@gmail.com>
2020-04-11 19:30:08 +0530

    adding button gradient hover effect

--- client/src/js/Login.js
+++ client/src/js/Login.js
@@ -40,29 +40,36 @@ const Login = props => {
           </div>
       </section>
       <section className="login-form">
-        <form onSubmit = {handleSubmit(onSubmit)}>
-          <div>
-            <label htmlFor = "userName">Username</label>
-            <input name = "userName" ref = {register({ required: true })} />
-            <span className = "error">
-              {errors.userName && "User name is required."}
-            </span>
-          </div>
+        <div className="login-form__body">
+          <h2 className="login-form__title">Welcome</h2>
+          <p>Please login to continue using Vento</p>
+          <form onSubmit = {handleSubmit(onSubmit)}>
+            <div className="form-group">
+              <label htmlFor = "userName">Username</label>
+              <input className="form-control" name = "userName" ref = {register({ required: true })} />
+              <span className = "error">
+                {errors.userName && "User name is required."}
+              </span>
+            </div>
 
-          <div>
-            <label htmlFor = "password">Password</label>
-            <input
-              name = "password"
-              type = "password"
-              ref = {register({ required: true })}
-            />
-            <span className = "error">
-              {errors.password && "Password is required."}
-            </span>
-          </div>
+            <div className="form-group">
+              <label htmlFor = "password">Password</label>
+              <input
+                className="form-control"
+                name = "password"
+                type = "password"
+                ref = {register({ required: true })}
+              />
+              <span className = "error">
+                {errors.password && "Password is required."}
+              </span>
+            </div>
 
-          <input type = "submit" />
-        </form>
+            <button type = "submit" className="btn btn-primary btn-primary--lg btn-block">
+              <div className="btn__value">Login</div>
+            </button>
+          </form>
+        </div>
       </section>
     </main>
   );
